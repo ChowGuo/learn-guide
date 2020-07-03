e15686c63b746451df12d368a5e90e52f597888d
Lynn <lynnguo42@outlook.com>
2020-07-03 13:55:10 +0800

    ByteBuffer 补充

--- src/main/java/org/hill/learnguide/nio/ByteBufferDemo.java
+++ src/main/java/org/hill/learnguide/nio/ByteBufferDemo.java
@@ -1,5 +1,7 @@
 package org.hill.learnguide.nio;
 
+import org.junit.Test;
+
 import java.nio.ByteBuffer;
 
 /**
@@ -7,12 +9,14 @@ import java.nio.ByteBuffer;
  * byte char short int long float double 基本类型除了 Boolean 都有对应的Buffer
  * 0 <= mark <= position <= limit <= capacity
  * 熟悉使用 Buffer 中的 position limit capacity , mark reset rewind clear 方法
+ *
+ * 直接缓冲区 与  非直接缓冲区
+ * 非直接缓冲区：通过allocate()方法分配缓冲区，将缓冲区建立在 JVM 上
+ * 直接缓冲区：通过allocateRedirect()方法分配直接缓冲区，将缓冲区建立在物理内存中， 可以提高效率。
  */
 public class ByteBufferDemo {
 
     public static void main(String[] args) {
-
-        test();
 
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         System.out.println("----------allocate()-------");
@@ -54,8 +58,8 @@ public class ByteBufferDemo {
         System.out.println("byteBuffer 位置：" + byteBuffer.position());
     }
 
-
-    private static void test() {
+    @Test
+    public void testBuf() {
         String str = "guochao";
         ByteBuffer byteBuffer = ByteBuffer.allocate(16);
         byteBuffer.put(str.getBytes());
@@ -80,4 +84,11 @@ public class ByteBufferDemo {
 
         System.out.println("############################");
     }
+
+    @Test
+    public void testDirectBuffer() {
+        ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+        System.out.println("是否是直接缓冲区：" + buf.isDirect());
+    }
+
 }
