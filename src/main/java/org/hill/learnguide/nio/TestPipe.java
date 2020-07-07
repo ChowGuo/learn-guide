b6270d9cfa0cf1a9f40497603d01f91c41b6e3b2
Lynn <lynnguo42@outlook.com>
2020-07-07 14:24:02 +0800

    管道pipe使用的内容修改

--- src/main/java/org/hill/learnguide/nio/TestPipe.java
+++ src/main/java/org/hill/learnguide/nio/TestPipe.java
@@ -5,21 +5,16 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 import java.nio.channels.Pipe;
 
-/**
- * @Description 测试管道
- * @Author 强风拂面
- * @Date 2020-7-7 10:20
- **/
 public class TestPipe {
     @Test
     public void testPipe() throws Exception{
         // 获取管道
         Pipe pipe = Pipe.open();
 
-        // 分配缓冲区
-
         // 将缓冲区中的数据写入到管道
         Pipe.SinkChannel sinkChannel = pipe.sink();
+
+        // 分配缓冲区
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         byteBuffer.put("通过sinkChannel写入数据".getBytes());
         byteBuffer.flip();
